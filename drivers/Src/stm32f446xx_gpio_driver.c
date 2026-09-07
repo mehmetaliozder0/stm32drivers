98d05cad63da8a1ec96a55bc1eec1f28ecd01212
Mehmet Ali Ozder <ozder.ali@metu.edu.tr>
2026-09-07 21:49:45 +0300

    fix(gpio): change IRQPriority type to uint32_t to prevent signed overflow

--- drivers/Src/stm32f446xx_gpio_driver.c
+++ drivers/Src/stm32f446xx_gpio_driver.c
@@ -229,7 +229,7 @@ void GPIO_IRQInterruptConfig(uint8_t IRQNumber, uint8_t EnOrDi){
 	}
 }
 
-void GPIO_IRQPriorityConfig(uint8_t IRQNumber, uint8_t IRQPriority){
+void GPIO_IRQPriorityConfig(uint8_t IRQNumber, uint32_t IRQPriority){
 	uint8_t iprx = IRQNumber / 4;
 	uint8_t iprx_section = IRQNumber % 4;
 	uint8_t shift_amount = (8 * iprx_section + (8-NO_PR_BITS_IMPLEMENTED));
